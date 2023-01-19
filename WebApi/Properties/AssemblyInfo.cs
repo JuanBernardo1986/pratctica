2429bf77f2b95ecde41d9809b2fadb4a7a8b3a73
JuanBernardo1986 <123032644+JuanBernardo1986@users.noreply.github.com>
2023-01-19 10:51:52 -0500

    Add files via upload

---
+++ WebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+/
+[assembly: AssemblyTitle("WebApi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApi")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("6ca7cc4f-6be2-4501-bec9-c4909e72537d")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
